0971c8c27cf3e513ff5fa55e2b269b5e6cae7458
swapbhosale <swapiiibhosale@gmail.com>
2021-08-09 12:19:49 +0000

    Our first java code push

---
+++ src/com/tcs/entity/User.java
@@ -0,0 +1,29 @@
+package com.tcs.entity;
+
+public class User {
+	private String name;
+	private Long mobileNumber;
+	private byte age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public byte getAge() {
+		return age;
+	}
+	public void setAge(byte age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[name="+name+"]";	}
+}
