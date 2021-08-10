55851c1be552654ee551a954c41f806f83c7c9d4
swapbhosale <swapiiibhosale@gmail.com>
2021-08-10 10:22:51 +0000

    day2

--- src/com/tcs/entity/VarArgsDemo.java
+++ src/com/tcs/entity/VarArgsDemo.java
@@ -5,6 +5,9 @@ public class VarArgsDemo {
 		String[] subjects = {"Math" , "English"};
 		Student student =new Student(subjects);
 		System.out.println(student.subjects[0]);
+		
+		Student student1 =new Student("Science");
+		System.out.println(student1.subjects[0]);
 		}
 }
 class Student{
