fa36ffddaa638bb851440a5a46a6768fa4676be5
swapbhosale <swapiiibhosale@gmail.com>
2021-08-10 09:06:55 +0000

    day2

--- src/com/tcs/entity/staticDemo.java
+++ src/com/tcs/entity/staticDemo.java
@@ -3,16 +3,18 @@ package com.tcs.entity;
 public class staticDemo {
 	public static void main(String[] args) {
 		Bank bank=new Bank();
-		Bank.createNewAccountNumber();
+		System.out.println(Bank.createNewAccountNumber());
+		System.out.println(bank.instanceVariable);
 		Bank.createNewAccountNumber();
 		
 	}
 }
 class Bank{
 	static int counter;
-	public static void createNewAccountNumber() {
-		++counter;
-		System.out.println(counter);
+	int instanceVariable=10;
+	public static int createNewAccountNumber() {
+		return ++counter ;
+		
 		
 	}
 	
