576544bdd40593e9a4f0c4ffcb1d5371f85f3b26
swapbhosale <swapiiibhosale@gmail.com>
2021-08-10 10:12:27 +0000

    day2

---
+++ src/com/tcs/entity/ArrayDemo.java
@@ -0,0 +1,25 @@
+package com.tcs.entity;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		EnhacnedForLoopDemo();
+	}
+
+	private static void EnhacnedForLoopDemo() {
+		int[] number =(3,4,-1,2,6);
+		for(int number : numbers) {
+			System.out.println(number);
+			break;
+		}
+	}
+
+	private static void demo1() {
+		int[] numbers= new int[10];
+		numbers[0] = 3;
+		numbers[4] = 1;
+		Arrays.sort(numbers,0,numbers.length());
+		
+	}
+}
