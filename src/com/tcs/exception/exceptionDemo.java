3620b380baf02c700a1495bc00b4294601ff279b
swapbhosale <swapiiibhosale@gmail.com>
2021-08-11 07:29:10 +0000

    day3

---
+++ src/com/tcs/exception/exceptionDemo.java
@@ -0,0 +1,14 @@
+package com.tcs.exception;
+
+public class exceptionDemo {
+	public static void main(String[] args) {
+		try {
+		System.out.println(1/0);
+	}
+		catch(ArithmeticException ex) {
+			ex.printStackTrace();
+			System.out.println("Divison by zero");
+		}
+		}
+
+}
